be1917489dde24baccd623f80161d9ea3b216c03
Nathan Green <ngreen@inco5.com>
2017-04-04 10:58:04 -0500

    Make src/ and build/ so .class and .java files do not mingle

--- edu/hendrix/grambler/grammars/RightNum.java
+++
@@ -1,9 +0,0 @@
-package edu.hendrix.grambler.grammars;
-
-public class RightNum extends edu.hendrix.grambler.Grammar {
-    public RightNum() {
-        super();
-        addProduction("num", new String[]{"\"[0-9]\"", "num"}, new String[]{"'.'"});
-    }
-}
-
--- edu/hendrix/grambler/gui/ChartPanelListener.java
+++
@@ -1,7 +0,0 @@
-package edu.hendrix.grambler.gui;
-
-import edu.hendrix.grambler.Chart;
-
-public interface ChartPanelListener {
-	public void rowChanged(int newRow, Chart c);
-}
